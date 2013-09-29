0251b31527380bce652e8bb5576de058429a6291
Jamie Bicknell <git@jamiebicknell.com>
2013-09-29 16:09:12 +0100

    Removed user agent

--- includes/XboxAPI.php
+++ includes/XboxAPI.php
@@ -5,7 +5,6 @@ class XboxAPI {
     function profile($gamertag) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'https://xboxapi.com/profile/' . urlencode($gamertag));
-        curl_setopt($ch, CURLOPT_USERAGENT, trim($command));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 15);
         curl_setopt($ch, CURLOPT_SSLVERSION, 3);
