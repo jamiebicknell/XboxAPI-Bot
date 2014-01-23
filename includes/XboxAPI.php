991f416d0f83fd5aa7dc9663be53cb606f54436c
Jamie Bicknell <git@jamiebicknell.com>
2014-01-23 23:47:26 +0000

    Updated API endpoints

--- includes/XboxAPI.php
+++ includes/XboxAPI.php
@@ -4,7 +4,7 @@ class XboxAPI {
 
     function limit() {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://xboxapi.com/limit');
+        curl_setopt($ch, CURLOPT_URL, 'https://xboxapi.com/v1/limit');
         curl_setopt($ch, CURLOPT_USERAGENT, 'XboxAPI Bot');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 15);
@@ -22,7 +22,7 @@ class XboxAPI {
 
     function profile($gamertag) {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://xboxapi.com/profile/' . urlencode($gamertag));
+        curl_setopt($ch, CURLOPT_URL, 'https://xboxapi.com/v1/profile/' . urlencode($gamertag));
         curl_setopt($ch, CURLOPT_USERAGENT, 'XboxAPI Bot');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 15);
